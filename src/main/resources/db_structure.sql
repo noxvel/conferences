54f624d35e38d6045a52877e57b16757257e26e6
noxvel <noxvel101@gmail.com>
2022-12-19 11:37:12 +0200

    added database model and sql files

---
+++ src/main/resources/db_structure.sql
@@ -0,0 +1,139 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema conferences
+-- -----------------------------------------------------
+DROP SCHEMA IF EXISTS `conferences` ;
+
+-- -----------------------------------------------------
+-- Schema conferences
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `conferences` DEFAULT CHARACTER SET utf8 ;
+USE `conferences` ;
+
+-- -----------------------------------------------------
+-- Table `conferences`.`user_role`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `conferences`.`user_role` ;
+
+CREATE TABLE IF NOT EXISTS `conferences`.`user_role` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `name_UNIQUE` (`name` ASC) VISIBLE)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `conferences`.`user`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `conferences`.`user` ;
+
+CREATE TABLE IF NOT EXISTS `conferences`.`user` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `email` VARCHAR(255) NOT NULL,
+  `password` VARCHAR(32) NOT NULL,
+  `user_role_id` INT NOT NULL,
+  `first_name` VARCHAR(255) NULL,
+  `last_name` VARCHAR(255) NULL,
+  `create_time` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE,
+  INDEX `fk_user_user_role_idx` (`user_role_id` ASC) VISIBLE,
+  CONSTRAINT `fk_user_user_role`
+    FOREIGN KEY (`user_role_id`)
+    REFERENCES `conferences`.`user_role` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
+
+
+-- -----------------------------------------------------
+-- Table `conferences`.`event`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `conferences`.`event` ;
+
+CREATE TABLE IF NOT EXISTS `conferences`.`event` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(255) NOT NULL,
+  `place` VARCHAR(255) NOT NULL,
+  `begin_date` DATETIME NOT NULL,
+  `end_date` DATETIME NOT NULL,
+  `participants_came` INT ZEROFILL NULL,
+  `description` VARCHAR(4096) NULL,
+  PRIMARY KEY (`id`));
+
+
+-- -----------------------------------------------------
+-- Table `conferences`.`report_status`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `conferences`.`report_status` ;
+
+CREATE TABLE IF NOT EXISTS `conferences`.`report_status` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(128) NOT NULL,
+  PRIMARY KEY (`id`));
+
+
+-- -----------------------------------------------------
+-- Table `conferences`.`report`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `conferences`.`report` ;
+
+CREATE TABLE IF NOT EXISTS `conferences`.`report` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `topic` VARCHAR(255) NOT NULL,
+  `speaker_id` INT NULL,
+  `event_id` INT NOT NULL,
+  `report_status_id` INT NOT NULL,
+  `description` VARCHAR(4096) NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_report_user1_idx` (`speaker_id` ASC) VISIBLE,
+  INDEX `fk_report_report_status1_idx` (`report_status_id` ASC) VISIBLE,
+  INDEX `fk_report_event1_idx` (`event_id` ASC) VISIBLE,
+  CONSTRAINT `fk_report_user1`
+    FOREIGN KEY (`speaker_id`)
+    REFERENCES `conferences`.`user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_report_report_status1`
+    FOREIGN KEY (`report_status_id`)
+    REFERENCES `conferences`.`report_status` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_report_event1`
+    FOREIGN KEY (`event_id`)
+    REFERENCES `conferences`.`event` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
+
+
+-- -----------------------------------------------------
+-- Table `conferences`.`event_has_participant`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `conferences`.`event_has_participant` ;
+
+CREATE TABLE IF NOT EXISTS `conferences`.`event_has_participant` (
+  `event_id` INT NOT NULL,
+  `user_id` INT NOT NULL,
+  PRIMARY KEY (`event_id`, `user_id`),
+  INDEX `fk_event_has_user_user1_idx` (`user_id` ASC) VISIBLE,
+  INDEX `fk_event_has_user_event1_idx` (`event_id` ASC) VISIBLE,
+  CONSTRAINT `fk_event_has_user_event1`
+    FOREIGN KEY (`event_id`)
+    REFERENCES `conferences`.`event` (`id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  CONSTRAINT `fk_event_has_user_user1`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `conferences`.`user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE CASCADE);
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
---
+++ src/main/resources/db_test_data.sql
@@ -0,0 +1,70 @@
+USE `conferences` ;
+
+-- TEST DATA
+-- -----------------------------------------------------
+INSERT INTO user_role (id, name)  VALUES (DEFAULT, 'MODERATOR');
+INSERT INTO user_role (id, name)  VALUES (DEFAULT, 'SPEAKER');
+INSERT INTO user_role (id, name)  VALUES (DEFAULT, 'USER');
+-- -----------------------------------------------------
+INSERT INTO user (id, email, password, user_role_id, first_name, last_name) VALUES (DEFAULT, 'moderator@mail.com', md5('123'), 1, 'Іван', 'Гармата');
+INSERT INTO user (id, email, password, user_role_id, first_name, last_name) VALUES (DEFAULT, 'speaker@mail.com', md5('123'), 2, 'Богдан', 'Хмельницький');
+INSERT INTO user (id, email, password, user_role_id, first_name, last_name) VALUES (DEFAULT, 'user@mail.com', md5('123'), 3, 'Микола', 'Пупкін');
+INSERT INTO user (id, email, password, user_role_id, first_name, last_name) VALUES (DEFAULT, 'user2@mail.com', md5('123'), 3, 'Леся', 'Українка');
+-- -----------------------------------------------------
+INSERT INTO report_status (id, name)  VALUES (DEFAULT, 'FREE');
+INSERT INTO report_status (id, name)  VALUES (DEFAULT, 'OFFERED_BY_SPEAKER');
+INSERT INTO report_status (id, name)  VALUES (DEFAULT, 'PROPOSE_TO_SPEAKER');
+INSERT INTO report_status (id, name)  VALUES (DEFAULT, 'SUGGESTED_SPEAKER');
+INSERT INTO report_status (id, name)  VALUES (DEFAULT, 'CONFIRMED');
+-- -----------------------------------------------------
+INSERT INTO event (id, name, place, begin_date, end_date,participants_came)  VALUES (DEFAULT, 'Java conference', 'Odessa', '2022-02-18 13:00:00', '2022-02-21 12:00:00',500);
+INSERT INTO event (id, name, place, begin_date, end_date)  VALUES (DEFAULT, 'C++ conference', 'San Francisco', '2022-07-04 16:00:00', '2022-07-08 13:00:00');
+INSERT INTO event (id, name, place, begin_date, end_date,participants_came)  VALUES (DEFAULT, 'Python conference', 'London', '2022-06-13 11:00:00', '2022-06-15 16:00:00',200);
+INSERT INTO event (id, name, place, begin_date, end_date,participants_came)  VALUES (DEFAULT, 'Ruby conference', 'Paris', '2022-08-22 16:00:00', '2022-08-24 17:30:00',300);
+INSERT INTO event (id, name, place, begin_date, end_date)  VALUES (DEFAULT, 'Kotlin conference', 'New York', '2022-04-12 08:00:00', '2022-04-14 16:00:00');
+INSERT INTO event (id, name, place, begin_date, end_date)  VALUES (DEFAULT, 'Scala conference', 'Rio de Janeiro', '2022-01-14 08:00:00', '2022-01-15 16:00:00');
+INSERT INTO event (id, name, place, begin_date, end_date,participants_came)  VALUES (DEFAULT, 'Android conference', 'Los Angeles', '2022-11-06 08:00:00', '2022-11-09 16:00:00',150);
+-- INSERT INTO event (id, name, place, begin_date, end_date)  VALUES (DEFAULT, 'Rust conference', 'Tokyo', '2022-07-17 08:00:00', '2022-07-19 16:00:00');
+INSERT INTO event (id, name, place, begin_date, end_date)  VALUES (DEFAULT, 'JavaScript conference', 'Helsinki', '2022-12-12 08:00:00', '2022-12-16 16:00:00');
+
+-- -----------------------------------------------------
+INSERT INTO report (id, topic, speaker_id, report_status_id, event_id)  VALUES (DEFAULT, 'New in Java', NULL, 1, 1);
+INSERT INTO report (id, topic, speaker_id, report_status_id, event_id)  VALUES (DEFAULT, 'Old in Java', 2, 4, 1);
+
+INSERT INTO report (id, topic, speaker_id, report_status_id, event_id)  VALUES (DEFAULT, 'New in C++', NULL, 1, 2);
+INSERT INTO report (id, topic, speaker_id, report_status_id, event_id)  VALUES (DEFAULT, 'Old in C++', 2, 2, 2);
+
+INSERT INTO report (id, topic, speaker_id, report_status_id, event_id)  VALUES (DEFAULT, 'New in Python', NULL, 1, 3);
+INSERT INTO report (id, topic, speaker_id, report_status_id, event_id)  VALUES (DEFAULT, 'Old in Python', 2, 3, 3);
+INSERT INTO report (id, topic, speaker_id, report_status_id, event_id)  VALUES (DEFAULT, 'Types in Python', NULL, 2, 3);
+INSERT INTO report (id, topic, speaker_id, report_status_id, event_id)  VALUES (DEFAULT, 'Test report 1', NULL, 1, 3);
+INSERT INTO report (id, topic, speaker_id, report_status_id, event_id)  VALUES (DEFAULT, 'Test report 2', 2, 3, 3);
+INSERT INTO report (id, topic, speaker_id, report_status_id, event_id)  VALUES (DEFAULT, 'Test report 3', NULL, 2, 3);
+INSERT INTO report (id, topic, speaker_id, report_status_id, event_id)  VALUES (DEFAULT, 'Test report 4', NULL, 1, 3);
+INSERT INTO report (id, topic, speaker_id, report_status_id, event_id)  VALUES (DEFAULT, 'Test report 5', 2, 3, 3);
+
+-- INSERT INTO report (id, topic, speaker_id, report_status_id, event_id)  VALUES (DEFAULT, 'New in Ruby', NULL, 1, 4); 
+-- INSERT INTO report (id, topic, speaker_id, report_status_id, event_id)  VALUES (DEFAULT, 'Old in Ruby', 2, 4, 4);
+
+INSERT INTO report (id, topic, speaker_id, report_status_id, event_id)  VALUES (DEFAULT, 'New in Kotlin', NULL, 1, 5);
+INSERT INTO report (id, topic, speaker_id, report_status_id, event_id)  VALUES (DEFAULT, 'Old in Kotlin', 2, 5, 5);
+INSERT INTO report (id, topic, speaker_id, report_status_id, event_id)  VALUES (DEFAULT, 'Async programming', NULL, 1, 5);
+
+INSERT INTO report (id, topic, speaker_id, report_status_id, event_id)  VALUES (DEFAULT, 'New in Scala', NULL, 1, 6);
+
+INSERT INTO report (id, topic, speaker_id, report_status_id, event_id)  VALUES (DEFAULT, 'New in Android', NULL, 1, 7);
+INSERT INTO report (id, topic, speaker_id, report_status_id, event_id)  VALUES (DEFAULT, 'Old in Android', 2, 3, 7);
+
+INSERT INTO report (id, topic, speaker_id, report_status_id, event_id)  VALUES (DEFAULT, 'New in JavaScript', NULL, 1, 8);
+
+
+-- -----------------------------------------------------
+INSERT INTO event_has_participant (event_id, user_id)  VALUES (1, 3);
+INSERT INTO event_has_participant (event_id, user_id)  VALUES (2, 3);
+INSERT INTO event_has_participant (event_id, user_id)  VALUES (4, 3);
+INSERT INTO event_has_participant (event_id, user_id)  VALUES (2, 4);
+INSERT INTO event_has_participant (event_id, user_id)  VALUES (3, 4);
+INSERT INTO event_has_participant (event_id, user_id)  VALUES (4, 4);
+
+
+
